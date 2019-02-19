cf4c543b38c366163184ace7988bfa6ba81d85b7
Pavel Belevtsev <pbelevtsev@hotmail.com>
2019-02-19 19:11:05 +0200

    Initial commit

---
+++ LoginDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LoginDemo
+//
+//  Created by Pavel Belevtsev on 2/19/19.
+//  Copyright © 2019 Pavel Belevtsev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
